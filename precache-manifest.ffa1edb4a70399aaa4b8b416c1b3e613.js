2ffcb1921515910bcdb0a14af9dd4c3ccca596c7
Gavin Cao <gavincao2017@hotmail.com>
2023-03-06 23:13:50 +1300

    Updates

---
+++ precache-manifest.ffa1edb4a70399aaa4b8b416c1b3e613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "160ba1c88c528beb0d9d",
+    "url": "/gavin.github.io/static/css/main.bcb6a410.chunk.css"
+  },
+  {
+    "revision": "160ba1c88c528beb0d9d",
+    "url": "/gavin.github.io/static/js/main.160ba1c8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/gavin.github.io/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/gavin.github.io/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "eda50940510e4fe8a545",
+    "url": "/gavin.github.io/static/js/runtime~main.eda50940.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gavin.github.io/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4972c55fff4fba246c3eb64b92e82577",
+    "url": "/gavin.github.io/index.html"
+  }
+];
