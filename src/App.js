c87f42704f8cf05a264651a76fd35e6dc85d18a7
Gavin Cao <gavincao2017@hotmail.com>
2023-03-07 21:41:16 +1300

    Prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, { Component }  from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CV from "./Gavin Cao.pdf";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Container">
+        <div className="title">Hey! It's me! Gavin!</div>
+
+        <a href={CV} target="_blank" rel="noopener noreferrer" download>
+          Download My CV
+        </a>
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/gcao417"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My GitHub Page
         </a>
-      </header>
+
+        <a
+          href="https://www.linkedin.com/in/gavin-cao-2a56381b8/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My LinkedIn
+        </a>
+
+        <a
+          href="mailto:gavincao2017@hotmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Email Me
+        </a>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// npm run deploy
+// npm start
+// type rfc+enter to create template for js file
